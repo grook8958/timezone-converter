5c1b797e7d3add47104a675617a985ab074b6510
grook8958 <78109323+grook8958@users.noreply.github.com>
2021-11-18 19:20:50 +0100

    Added the most common timezone abbreviations

---
+++ src/Util/Constants.js
@@ -0,0 +1,19 @@
+exports.TimezoneAbbreviations = {
+    AEST: "UTC+10",
+    ACST: "UTC+9:30",
+    AFT: "UTC+4:30",
+    AKST: "UTC-9",
+    AST: "UTC-4",
+    AWST: "UTC+9",
+    CAT: "UTC+2",
+    CET: "UTC+1",
+    CST: "UTC-6",
+    EAT: "UTC+3",
+    EET: "UTC+2",
+    EST: "UTC-5",
+    MSK: "UTC+3",
+    MST: "UTC-7",
+    PST: "UTC-8",
+    WAT: "UTC+1",
+    WET: "UTC+0"
+}
